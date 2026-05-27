864838e74a867fc1b6e1a3cf798eb0e3a73e9d52
Matheus Batista <matheusifro2020@gmail.com>
2026-05-27 14:25:23 -0400

    fix: update .env.example

--- src/utils/SendMail.js
+++ src/utils/SendMail.js
@@ -18,7 +18,7 @@ class SendMail {
       let transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
-        secure: false, // true para 465, false para outras portas
+        secure: process.env.EMAIL_SECURE === "true",
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
